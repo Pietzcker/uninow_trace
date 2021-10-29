5207c997f06e540e0baa1ffe81330c3ca7d9fd10
Pietzcker <tim@pietzckers.de>
2021-10-29 10:55:02 +0200

    Aufgeräumt, Datumsformat in Variablen überführt, Anzeige Übersicht über Datenumfang hinzugefügt

--- checkins.py
+++ checkins.py
@@ -30,12 +30,28 @@ def get_zeit(prompt):
 # checkins-Datei lesen - in Excel als CSV-Datei abgespeichert im gleichen Ordner
 # wie dieses Skript
 
+dformat = "%d.%m.%Y, %H:%M" # Datumsformat in der checkins-Datei
+kdformat = "%d.%m.%y %H:%M" # Kurz-Datumsformat für die Ausgabe
+
 with open("checkins.csv", newline="", encoding="cp1252") as csvfile:
     reader = csv.DictReader(csvfile, delimiter=";")
-    daten = list(reader)
+    # Text-Datumseinträge in Datumstypen umwandeln
+    daten = []
+    for eintrag in reader:
+        if not any(eintrag.values()): # Leerzeilen überspringen
+            continue
+        eintrag["checked_in_at"] = datetime.datetime.strptime(eintrag["checked_in_at"], dformat)
+        eintrag["checked_out_at"] = datetime.datetime.strptime(eintrag["checked_out_at"], dformat)
+        daten.append(eintrag)
 
 räume = set(eintrag["room"] for eintrag in daten)
+anzahl = len(daten)
+min_datum = min(daten, key = lambda l: l["checked_in_at"])["checked_in_at"]
+max_datum = max(daten, key = lambda l: l["checked_out_at"])["checked_out_at"]
 
+print("{} Datensätze; abgedeckter Datumsbereich {} bis {}".format(anzahl,
+                                                                  datetime.datetime.strftime(min_datum, kdformat),
+                                                                  datetime.datetime.strftime(max_datum, kdformat)))
 matr_index = input("Welche Matrikelnummer hat der Indexfall? ")
 
 sd_index = get_datum("Wann war der positive Test bzw. Symptombeginn (TT.MM.JJJJ)? ")
@@ -50,18 +66,18 @@ print(f"Diese Einträge in der Datei seit dem {sd_text} habe ich gefunden:")
 for eintrag in daten:
     if matr_index == eintrag["external_id"]:
         checkin = {}
-        checkin["ein"] = datetime.datetime.strptime(eintrag["checked_in_at"], "%d.%m.%Y, %H:%M")
+        checkin["ein"] = eintrag["checked_in_at"]
         if checkin["ein"] > suchdatum_index:
             checkin["raum"] = eintrag["room"]
-            checkin["aus"] = datetime.datetime.strptime(eintrag["checked_out_at"], "%d.%m.%Y, %H:%M")
+            checkin["aus"] = eintrag["checked_out_at"]
             checkins.append(checkin)
 
 checkins.sort(key=lambda l: l["ein"])
 print(f"Checkins für Matrikelnummer {matr_index}:")
 for checkin in checkins:
     print("Check-in in Raum {} von {} bis {} (Dauer: {})".format(checkin["raum"],
-                                                                 datetime.datetime.strftime(checkin["ein"], "%d.%m.%y %H:%M"), 
-                                                                 datetime.datetime.strftime(checkin["aus"], "%d.%m.%y %H:%M"),
+                                                                 datetime.datetime.strftime(checkin["ein"], kdformat), 
+                                                                 datetime.datetime.strftime(checkin["aus"], kdformat),
                                                                  checkin["aus"] - checkin["ein"]))
 while True:
     datum = get_datum("Gibt es weitere Checkins, die nicht erkannt wurden?\nWenn nein, ENTER drücken.\nWenn ja, wann (TT.MM.JJJJ)? ")
@@ -85,8 +101,8 @@ kontakte = defaultdict(list)
 for checkin in checkins:
     for eintrag in daten:
         if eintrag["room"] == checkin["raum"] and eintrag["external_id"] != matr_index:
-            kontakt_ein = datetime.datetime.strptime(eintrag["checked_in_at"], "%d.%m.%Y, %H:%M")
-            kontakt_aus = datetime.datetime.strptime(eintrag["checked_out_at"], "%d.%m.%Y, %H:%M")
+            kontakt_aus = eintrag["checked_out_at"]
+            kontakt_ein = eintrag["checked_in_at"]
             if checkin["ein"] <= kontakt_aus and checkin["aus"] >= kontakt_ein:
                 start_kontakt = max(checkin["ein"], kontakt_ein)
                 end_kontakt = min(checkin["aus"], kontakt_aus)
@@ -95,7 +111,7 @@ for checkin in checkins:
 for kontakt, details in kontakte.items():
     print(f"Kontakte mit {kontakt}:")
     for detail in details:
-        print(f'  {detail[0]} von {datetime.datetime.strftime(detail[1], "%d.%m.%y %H:%M")} bis {datetime.datetime.strftime(detail[2], "%d.%m.%y %H:%M")} ({detail[2]-detail[1]})')
+        print(f'  {detail[0]} von {datetime.datetime.strftime(detail[1], kdformat)} bis {datetime.datetime.strftime(detail[2], kdformat)} ({detail[2]-detail[1]})')
 
 print(f"Liste aller Kontaktpersonen seit dem {sd_text}:")
 for kontakt in kontakte:
